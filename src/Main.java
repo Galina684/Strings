90d6a2f0d89cb2d976ece503a2dd2628e3a1ce27
Galina684 <galina684@gmail.com>
2023-07-18 12:39:27 +0300

    Домашка по строкам задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 
 public class Main {
     public static void main(String[] args) {
-        //домашка по теме String
+
+
+        System.out.println("Задача ");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+
+        System.out.println("Задача 2");
+
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("Задача 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета " + fullName);
     }
 }
